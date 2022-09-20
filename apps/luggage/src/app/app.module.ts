3b4ed8ccbe7f5251240bcbad7b084ccd0ff3a322
Sophia Brandt <git@s-br.net>
2022-09-20 18:18:12 +0200

    feat: add luggage app with 2 features via DDD plugin

---
+++ apps/luggage/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@NgModule({
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'check-in'
+      }, {
+        path: 'check-in',
+        loadChildren: () => import('@flight-workspace/luggage/feature-checkin')
+          .then(m => m.LuggageFeatureCheckinModule)
+      },
+      {
+        path: 'report-loss',
+        loadChildren: () => import('@flight-workspace/luggage/feature-report-loss')
+          .then(m => m.LuggageFeatureReportLossModule)
+      }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {
+}
