5454ae564c4201c5971ec6791cebadd497fb1775
Sophia Brandt <git@s-br.net>
2022-09-21 08:28:42 +0200

    feat: add ui card

--- libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
+++ libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
+import { LuggageUiCardModule } from '@flight-workspace/luggage/ui-card';
 import { CheckinComponent } from './checkin.component';
 
 @NgModule({
   imports: [CommonModule, LuggageDomainModule,
     RouterModule.forChild([
       { path: '', component: CheckinComponent }
-    ])
+    ]),
+    LuggageUiCardModule
   ],
   declarations: [CheckinComponent],
   exports: [CheckinComponent],
