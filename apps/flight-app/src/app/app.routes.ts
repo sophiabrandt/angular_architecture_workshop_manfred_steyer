d81883ba1b9521a4d1955ad415c7dbd54b85d294
Sophia Brandt <git@s-br.net>
2022-09-21 16:55:50 +0200

    feat: use dynamic module federation with web components

--- apps/flight-app/src/app/app.routes.ts
+++ apps/flight-app/src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { startsWith, WebComponentWrapper, WebComponentWrapperOptions } from '@angular-architects/module-federation-tools';
 import { Routes } from '@angular/router';
 import { loadRemoteModule } from '@angular-architects/module-federation'
 import { PassengerMf } from '../mf-types';
@@ -28,6 +29,40 @@ export const APP_ROUTES: Routes = [
     })
       .then(esm => esm.PassengerModule)
   },
+  {
+    path: 'angular2',
+    component: WebComponentWrapper,
+    data: {
+      remoteEntry: 'https://gray-pond-030798810.azurestaticapps.net//remoteEntry.js',
+      remoteName: 'angular2',
+      exposedModule: './web-components',
+      elementName: 'angular2-element'
+    } as WebComponentWrapperOptions
+  },
+
+  // And this route too:
+  {
+    path: 'react',
+    component: WebComponentWrapper,
+    data: {
+      remoteEntry: 'https://witty-wave-0a695f710.azurestaticapps.net/remoteEntry.js',
+      remoteName: 'react',
+      exposedModule: './web-components',
+      elementName: 'react-element'
+    } as WebComponentWrapperOptions
+  },
+
+  {
+    matcher: startsWith('angular3'),
+    component: WebComponentWrapper,
+    data: {
+      remoteEntry: 'https://gray-river-0b8c23a10.azurestaticapps.net/remoteEntry.js',
+      remoteName: 'angular3',
+      exposedModule: './web-components',
+      elementName: 'angular3-element'
+    } as WebComponentWrapperOptions
+  },
+
   {
     path: '**',
     redirectTo: 'home'
